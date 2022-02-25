3a9176e9ce0f967c39ba40c563297fe49a25a73d
Jesus Pombo <jesuspombo01@gmail.com>
2022-02-25 10:44:58 +0000

    First Part

---
+++ app/src/main/java/com/example/task04a/ViewOne.kt
@@ -0,0 +1,23 @@
+package com.example.task04a
+
+import android.content.Context
+import android.graphics.Color
+import android.util.AttributeSet
+import android.view.View
+
+class ViewOne : View {
+    constructor(context: Context?) : super(context)
+    constructor(context: Context?, attrs: AttributeSet?) : super(context, attrs)
+    constructor(context: Context?, attrs: AttributeSet?, defStyleAttr: Int) : super(
+        context,
+        attrs,
+        defStyleAttr
+    )
+
+    init {
+        // 'this' is a reference to the current class.
+        // Views have many properties - one is backgroundColor.
+        this.setBackgroundColor(Color.argb(128,32,64,255))
+    }
+
+}
